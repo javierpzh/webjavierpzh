432b55fd8d5b8c25a8692716c4eda7a12f8f5a55
Javier Pérez Hidalgo <javierperezhidalgo01@gmail.com>
2022-01-08 01:33:16 +0100

    new

--- upgradeRepository.sh
+++ upgradeRepository.sh
@@ -4,7 +4,9 @@
 
 echo " --> Adding changes"
 git add .
+echo ""
 read -p " -->	Enter the message of the commit: " nameCommit
 git commit -am $nameCommit
+echo ""
 echo " --> Uploading the changes"
 git push
