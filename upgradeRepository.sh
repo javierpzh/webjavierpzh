24850d297db9ffa8cba4a31a7c55625afbcbf558
Javier Pérez Hidalgo <javierperezhidalgo01@gmail.com>
2022-01-08 01:28:23 +0100

    new

--- upgradeRepository.sh
+++ upgradeRepository.sh
@@ -1,8 +1,8 @@
 # This script uploads the changes to the repository in only one command
 
-echo "Adding changes"
+echo " --> Adding changes"
 git add .
-read -p "Enter the message of the commit: " nameCommit
+read -p " -->	Enter the message of the commit: " nameCommit
 git commit -am $nameCommit
-echo "Uploading the changes"
+echo " --> Uploading the changes"
 git push
